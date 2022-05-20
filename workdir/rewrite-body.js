15a83d3fc27722fd7ee70df3c4f2b6b610603b3d
navigaid <navigaid@gmail.com>
2022-05-21 03:22:16 +0800

    add conf

--- workdir/rewrite-body.js
+++ workdir/rewrite-body.js
@@ -1,3 +1,6 @@
+/* [rewrite_local]
+ * ^https://xcx-static.yqgz.beijing.gov.cn/json/jkb/hsjc/dailyAppointment.json url script-response-body https://raw.githubusercontent.com/parodytech/Quantumult-X/master/workdir/rewrite-body.js
+ */
 var body = $response.body;
 var obj = JSON.parse(body);
 
