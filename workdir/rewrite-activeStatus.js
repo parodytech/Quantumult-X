f41f353d27da63e7ac14926b76db3c94b8bc6ccb
navigaid <navigaid@gmail.com>
2022-05-23 05:16:58 +0800

    rewrite activeStatus

---
+++ workdir/rewrite-activeStatus.js
@@ -0,0 +1,12 @@
+/* [rewrite_local]
+ * ^https://xcx-static.yqgz.beijing.gov.cn/json/jkb/hsjc/dailyAppointment.json url script-response-body https://raw.githubusercontent.com/parodytech/Quantumult-X/master/workdir/rewrite-body.js
+ */
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj['data']['activeStatus'] = "1";
+body = JSON.stringify(obj);
+
+console.log(body);
+
+$done(body);
