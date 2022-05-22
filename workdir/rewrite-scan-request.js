0ed019b53fcf716685acde9f9631e36d1ffdf4dd
navigaid <navigaid@gmail.com>
2022-05-23 06:12:44 +0800

    rewrite scan

---
+++ workdir/rewrite-scan-request.js
@@ -0,0 +1,13 @@
+/* [rewrite_local]
+ * ^https://xcx-static.yqgz.beijing.gov.cn/json/jkb/hsjc/dailyAppointment.json url script-response-body https://raw.githubusercontent.com/parodytech/Quantumult-X/master/workdir/rewrite-body.js
+ */
+var body = $request.body;
+var obj = JSON.parse(body);
+
+obj['qrcode_content'] = "";
+
+body = JSON.stringify(obj);
+
+console.log(body);
+
+$done(body);
